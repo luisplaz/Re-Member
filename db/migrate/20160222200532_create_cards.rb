03eeba3dd9c472bc8e540193f59f2e5f91ce2b7d
luisplaz <luisplaz@hotmail.com>
2016-02-23 15:29:21 -0500

    Fix indentation in card migration for better readability

--- db/migrate/20160222200532_create_cards.rb
+++ db/migrate/20160222200532_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-    	t.integer :deck_id, null: false, index: true
-    	t.string :name, null: false
-    	t.string :title, null: false
+    	t.integer :deck_id,    null: false, index: true
+    	t.string :name,        null: false
+    	t.string :title,       null: false
     	t.string :description, null: false
 
     	t.timestamps null:false
