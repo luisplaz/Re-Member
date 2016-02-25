5b340d88d08da2c4b4652fbb79874efaf4b0aace
luisplaz <luisplaz@hotmail.com>
2016-02-25 12:50:40 -0500

    Add routes to routes rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  
+  resources :users do
+    resources :decks
+  end
+
+  resources :decks do
+    resources :cards
+    resources :rounds
+  end
+
+  resources :rounds do
+    resources :guesses
+  end
+
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
