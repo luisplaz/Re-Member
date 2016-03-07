cd57807c44bf8994b924ac7d1a159b2376c40f13
luisplaz <luisplaz@hotmail.com>
2016-03-07 16:35:46 -0500

    Modify Schema to reflect migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -49,11 +49,11 @@ ActiveRecord::Schema.define(version: 20160222203646) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 20, null: false
-    t.string   "email",                 null: false
-    t.string   "password",              null: false
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
+    t.string   "username",        limit: 20, null: false
+    t.string   "email",                      null: false
+    t.string   "password_digest",            null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
