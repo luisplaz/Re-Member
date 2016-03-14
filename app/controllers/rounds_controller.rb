096fce86e66fb3e43cc67de1347fbffcf06c1986
luisplaz <luisplaz@hotmail.com>
2016-03-14 18:36:24 -0400

    Create show and create routes for rounds

---
+++ app/controllers/rounds_controller.rb
@@ -0,0 +1,13 @@
+class RoundsController < ApplicationController
+
+	def create
+		@round = Round.new(deck_id: params[:deck_id],user:current_user)
+		if @round.save
+			redirect_to round_path(id: @round.id)
+		end
+	end
+
+	def show
+		@round = Round.includes(:guesses, deck: [:cards]).find(params[:id])
+	end
+end
