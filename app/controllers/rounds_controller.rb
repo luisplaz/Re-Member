f99dc61f20f2d62d7fa586b2146040974c2fbd40
luisplaz <luisplaz@hotmail.com>
2016-03-15 17:57:41 -0400

    Fix redirect path in round controller

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,11 +3,12 @@ class RoundsController < ApplicationController
 	def create
 		@round = Round.new(deck_id: params[:deck_id],user:current_user)
 		if @round.save
-			redirect_to round_path(id: @round.id)
+			redirect_to round_path(@round)
 		end
 	end
 
 	def show
 		@round = Round.includes(:guesses, deck: [:cards]).find(params[:id])
 	end
+	
 end
