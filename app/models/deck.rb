8d37d143f1ae2a0f1596abbd583ed53c3b7b3838
luisplaz <luisplaz@hotmail.com>
2016-03-15 13:34:45 -0400

    Add method so cards associated to a deck are deleted when deck is deleted

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,9 +1,17 @@
 class Deck < ActiveRecord::Base
 
+	before_destroy :destroy_cards
+
 	belongs_to  :user
-	has_many    :cards
+	has_many    :cards, dependent: :delete_all
 	has_many    :rounds
 
 	validates :name, presence: true
+
+	private
+
+	def destroy_cards
+	  self.cards.delete_all   
+	end
 	
 end
