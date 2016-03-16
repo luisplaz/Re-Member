e71a8739083b70385c098a4eb693dc06f6e9a3a6
luisplaz <luisplaz@hotmail.com>
2016-03-16 13:41:12 -0400

    Add avatar attribute to card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,9 @@
 class Card < ActiveRecord::Base
-
+	
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	belongs_to               :deck
 	has_many                 :guesses
 	has_and_belongs_to_many  :rounds, :through => :guesses
 
+  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
