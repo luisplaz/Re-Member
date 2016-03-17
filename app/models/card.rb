4d7b5bdc5eabc740887335179229520627d4c08a
luisplaz <luisplaz@hotmail.com>
2016-03-17 15:04:52 -0400

    Organize code for better reading in card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ActiveRecord::Base
 	
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file        :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 	belongs_to               :deck
 	has_many                 :guesses
 	has_and_belongs_to_many  :rounds, :through => :guesses
