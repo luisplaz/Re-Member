eb77f75e35eed6137afcf2d871a646f1686bee1d
luisplaz <luisplaz@hotmail.com>
2016-03-22 17:03:43 -0400

    Add associations for users and cards and guesses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 	  has_secure_password
 	  has_many :decks
 	  has_many :rounds
+	  has_many :cards, through: :decks
+	  has_many :guesses, through: :rounds
 
 	  validates :username, presence: true, length: {in: 8..20}
 	  validates :email, presence: true
